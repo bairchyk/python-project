bbcace45e163dc9e8f7a1ab4978cab3529112dba
bairchyk <brakshaev7@bk.ru>
2026-09-12 19:34:55 +0300

    final fixes

--- src/widget.py
+++ src/widget.py
@@ -1,26 +1,18 @@
-card_info = input()
+from datetime import datetime
 
 
 def get_mask_card_number(card_number: str) -> str:
-    #Функция маскирует номер банковской карты.
+    """Функция маскирует номер банковской карты."""
     if len(card_number) != 16:
         return "Invalid card number"
 
     masked_card = card_number[:6] + "******" + card_number[12:]
 
-    return (
-        masked_card[:4]
-        + " "
-        + masked_card[4:8]
-        + " "
-        + masked_card[8:12]
-        + " "
-        + masked_card[12:]
-    )
+    return masked_card[:4] + " " + masked_card[4:8] + " " + masked_card[8:12] + " " + masked_card[12:]
 
 
 def get_mask_account(account_number: str) -> str:
-    #Функция маскирует номер банковского счета.
+    """Функция маскирует номер банковского счета."""
     if len(account_number) < 4:
         return "Invalid account number"
 
@@ -28,34 +20,42 @@ def get_mask_account(account_number: str) -> str:
 
 
 def mask_account_card(card_info: str) -> str:
-    #Функция маскирует номер карты или счета в зависимости от типа.
-    name, number = card_info.rsplit(" ", 1)
+    """Функция маскирует номер карты или счета в зависимости от типа."""
+
+    if not card_info.strip():
+        return "Invalid input"
+
+    parts = card_info.rsplit(" ", 1)
+
+    if len(parts) != 2:
+        return "Invalid input"
+
+    name, number = parts
 
     if name == "Счет":
         masked_number = get_mask_account(number)
     else:
         masked_number = get_mask_card_number(number)
 
-    return name + " " + masked_number
+    return str(name + " " + masked_number)
 
 
-print(mask_account_card(card_info))
+def get_date_withoutiso(date_input: str) -> str:
+    """Функция преобразует дату в формат ДД.ММ.ГГГГ. через split"""
+    date_separated = date_input.split("-")
 
-
-date_input = input()
+    return date_separated[2][:2] + "." + date_separated[1] + "." + date_separated[0]
 
 
 def get_date(date_input: str) -> str:
-    #Функция преобразует дату в формат ДД.ММ.ГГГГ.
-    date_separated = date_input.split("-")
-
-    return (
-        date_separated[2][:2]
-        + "."
-        + date_separated[1]
-        + "."
-        + date_separated[0]
-    )
+    """Функция преобразует дату в формат ДД.ММ.ГГГГ. для ISO формата"""
+    date = datetime.fromisoformat(date_input)
+    return date.strftime("%d.%m.%Y")
 
 
-print(get_date(date_input))
+if __name__ == "__main__":
+    card_info = input()
+    print(mask_account_card(card_info))
+
+    date_input = input()
+    print(get_date(date_input))
